849a230cf49959bdc06b6fb8f97ff0d10bccd77e
merveo1608 <147334271+merveo1608@users.noreply.github.com>
2024-07-13 16:08:30 +0300

    next
    kullanıcının oturum açma kontrolü website tarafında halledildi

--- NewsCenter/obj/Debug/net8.0/NewsCenter.AssemblyInfo.cs
+++ NewsCenter/obj/Debug/net8.0/NewsCenter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewsCenter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0e9dcf8e4822305fc5d52b96bf683cae94df6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a89a060ddebb94d7f5a852059a444ac24c4ebc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewsCenter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewsCenter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
